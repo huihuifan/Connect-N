2f3c79731696c1a01513382a562faf58c1a72096
Lucy Cheng <lcheng@college.harvard.edu>
2015-04-27 23:55:46 -0400

    modules

---
+++ helpers.py
@@ -0,0 +1,103 @@
+from __future__ import division
+import numpy as np
+from numpy.random import rand
+import math
+from random import randint
+import itertools
+import random
+import copy
+from copy import deepcopy
+from multiprocessing import Pool
+import matplotlib.pyplot as plt
+get_ipython().magic(u'matplotlib inline')
+
+import simulator
+import agents
+
+# TODO (LUCY): CLEAN THIS UP
+'''
+MODIFIED TO (NOT PRINT) AND (RETURN INDICATOR OF WINNER)
+'''
+def play_game_mod(board, p1, p2, q=False):
+    """
+    Runs Connect 4 game given simulator object and two agents (players)
+    """
+    reward = None
+
+    if q == True:
+        while True:
+            #print("p1")
+            p1move = p1.interact(reward, board)
+            #print(p1move)
+            if (p1move is None):
+                board.print_grid()
+                print("error player 1 a")
+                return -1, 0
+            p1result, reward = board.move(p1move, 1)
+            #print p1result
+            if (p1result == 1):
+                #print("player 1")
+                #board.print_grid()
+                return 1, 1
+            elif (p1result == -1):
+                board.print_grid()
+                print("error player 1 b")
+                return -1, 0
+            #print("p2")
+            p2move = p2.calc_next_move()
+            #print(p2move)
+            if (p2move is None):
+                board.print_grid()
+                print("error player 2")
+                return -1, 0
+            p2result = board.move(p2move, -1)
+            #print p2result
+            if (p2result[0] == 1):
+                #print("player 2")
+                #board.print_grid()
+                return 1, -1
+            elif (p2result[0] == -1):
+                board.print_grid()
+                print("error player 2")
+                return -1, 0
+
+    else:
+        while True:
+            #print("p1")
+            p1move = p1.calc_next_move()
+            #print(p1move, board.next_possible_moves())
+            #print(p1move)
+            if (p1move is None):
+                board.print_grid()
+                print("error 1")
+                return -1, 0
+            p1result = board.move(p1move, 1)
+            #print p1result
+            if (p1result[0] == 1):
+                #print("player 1")
+                #board.print_grid()
+                return 1, 1
+            elif (p1result[0] == -1):
+                board.print_grid()
+                print("error 2")
+                return -1, 0
+            #print("p2")
+            p2move = p2.calc_next_move()
+            #print(p2move, board.next_possible_moves())
+            #print(p2move)
+            if (p2move is None):
+                board.print_grid()
+                print("error 3")
+                return -1, 0
+            p2result = board.move(p2move, -1)
+            #print p2result
+            if (p2result[0] == 1):
+                #print("player 2")
+                #board.print_grid()
+                return 1, -1
+            elif (p2result[0] == -1):
+                board.print_grid()
+                print("error 4")
+                return -1, 0
+
+
---
+++ simulator.py
@@ -0,0 +1,168 @@
+from __future__ import division
+import numpy as np
+from numpy.random import rand
+import math
+from random import randint
+import itertools
+import random
+import copy
+from copy import deepcopy
+from multiprocessing import Pool
+
+
+class ConnectN:
+    """
+    Connect N game simulator for two players, 1 and -1.
+
+    Inputs:
+    Grid size- creates a grid size x grid size square board
+    N- number of tokens a player must connect to win the game
+
+    """
+
+    def __init__(self, grid_size, n):
+        self.n = n
+        self.grid_size = grid_size
+        self.grid = np.zeros([grid_size,grid_size])
+        self.finished = 0
+        self.turn_num = 0
+
+    def reset(self):
+        self.__init__(self.grid_size, self.n)
+
+    def check_win(self, col, row, player):
+        """
+        Checks if given player has connected N tokens.
+        """
+        for i in range(0, self.n):
+            if sum(self.grid[col, row - i:row - i + self.n]) == self.n*player:
+                self.finished = 1
+                return 1
+            if sum(self.grid[col - i: col - i + self.n, row]) == self.n*player:
+                self.finished = 1
+                return 1
+            if col - i >= 0 and col - i + self.n - 1 < self.grid_size and row - i >= 0 and row - i + self.n - 1 < self.grid_size:
+                if sum([self.grid[col - i + x, row - i + x] for x in range(0, self.n)]) == self.n*player:
+                    self.finished = 1
+                    return 1
+            if col - i >= 0 and col - i + self.n - 1 < self.grid_size and row + i >= self.n - 1 and row + i < self.grid_size:
+                if sum([self.grid[col - i + x, row + i - x] for x in range(0, self.n)]) == self.n*player:
+                    self.finished = 1
+                    return 1
+        return 0
+
+    def move(self, col, player):
+        """
+        Given player and column to move in, modifies board and increments the turn counter.
+
+        Returns a tuple, where first value is return message and second value is reward.
+        """
+        self.turn_num += 1
+
+        if self.finished == 1:
+            return 1, 50
+        sum_col = np.sum([abs(x) for x in self.grid[col]])
+        if sum_col == self.grid_size:
+            return -1, -1
+        self.grid[col, sum_col] = player
+        if self.check_win(col, sum_col, player) == 1:
+            return 1, 50
+        return 0, 0
+
+    def simulate_move(self, col, player):
+        """
+        Tests a move and returns if it is valid or not
+        """
+        sum_col = np.sum([abs(x) for x in self.grid[col]])
+        if sum_col == self.grid_size:
+            return 1
+        else:
+            return 0
+
+    def turn(self):
+        """
+        Returns which player's turn it is. First turn is player 1, second turn is player -1.
+        """
+        if self.turn_num%2 == 0:
+            return 1
+        else:
+            return -1
+
+    def next_possible_moves(self):
+        """
+        Returns array of possible columns for a next move
+        """
+        columns = []
+
+        for i in xrange(0, self.grid_size):
+            if (0 in self.grid[i]):
+                columns.append(i)
+
+        return columns
+
+    def all_tokens_placed(self):
+        """
+        Returns location of all tokens (column, row) that have been placed
+        """
+        all_tokens = []
+
+        for col in xrange(0, self.grid_size):
+            for row in xrange(0, self.grid_size):
+                if self.grid[col][row] != 0:
+                    all_tokens.append({"location": [col, row], "player": self.grid[col][row]})
+
+        return all_tokens
+
+    def is_empty(self, col, row):
+        """
+        Returns if a given spot (column, row) is empty
+        """
+        return self.grid[col][row] == 0
+
+    """
+    Following streak functions check if player has token streak in the four possible win directions
+    """
+    def streakVertical(self, board, col, row, player):
+        if row > len(board[col]) - self.n:
+            return 0
+        for i in range(0,self.n):
+            if board[col][row + i] == -1*player:
+                return 0
+            if board[col][row + i] == 0:
+                return i
+        return self.n
+
+    def streakHorizontal(self, board, col, row, player):
+        if col > len(board) - self.n:
+            return 0
+        for i in range(0,self.n):
+            if board[col + i][row] == -1*player:
+                return 0
+            if board[col + i][row] == 0:
+                return i
+        return self.n
+
+    def streakDiagonalUp(self, board, col, row, player):
+        if row > len(board[col]) - self.n or col > len(board) - self.n:
+            return 0
+        for i in range(0,self.n):
+            if board[col + i][row + i] == -1*player:
+                return 0
+            if board[col + i][row + i] == 0:
+                return i
+        return self.n
+
+    def streakDiagonalDown(self, board, col, row, player):
+        if row < self.n or col > len(board) - self.n:
+            return 0
+        for i in range(0,self.n):
+            if board[col + i][row - i] == -1*player:
+                return 0
+            if board[col + i][row - i] == 0:
+                return i
+        return self.n
+
+    def print_grid(self):
+        print(np.rot90(self.grid))
+
+
